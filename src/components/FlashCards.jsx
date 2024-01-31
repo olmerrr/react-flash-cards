660818b2a04523f2094ccf1b7b5b44e78678a202
Yaroslav Dombrovskiy <yaroslav.dombrovskyi@nixs.com>
2024-01-31 12:03:09 +0100

    Update click fn.

--- src/components/FlashCards.jsx
+++ src/components/FlashCards.jsx
@@ -1,5 +1,21 @@
-function FlashCards() {
-  return <div>TODO</div>;
+import { useState } from "react";
+
+function FlashCards({ questions }) {
+  const [ selectId, setSelectedId ] = useState(null);
+  const handleClick = (id) => setSelectedId(id !== selectId ? id : null);
+
+  return <ul className="flashcards">
+    {questions.map(q => (
+        <li
+          key={q.id}
+          className={q.id === selectId ? 'selected' : ''}
+          onClick={() => handleClick(q.id)}
+        >
+          {q.id === selectId ? q.answer : q.question}
+        </li>
+      )
+    )}
+  </ul>;
 }
 
 export default FlashCards;
